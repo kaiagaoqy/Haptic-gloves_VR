83deb1f4314153a6771b95b2e248032c881a7c7d
Kaia <kaiagaoqy@gmail.com>
2022-12-08 22:40:42 -0500

    Update source code

---
+++ firmware/firmware4leftHand/SerialCommunication.ino
@@ -0,0 +1,31 @@
+class SerialCommunication : public ICommunication {
+  private:
+    bool m_isOpen;
+
+  public:
+    SerialCommunication() {
+      m_isOpen = false;
+    }
+
+    bool isOpen(){
+      return m_isOpen;
+    }
+
+    void start(){
+      //Serial.setTimeout(1000000);
+      Serial.begin(SERIAL_BAUD_RATE);
+      m_isOpen = true;
+    }
+
+    void output(char* data){
+      Serial.print(data);
+      Serial.flush();
+    }
+
+    bool readData(char* input){
+      byte size = Serial.readBytesUntil('\n', input, 100);
+      // print(Serial.readStringUntil('\n'));
+      // input[size] = NULL;
+      return input != NULL && strlen(input) > 0;
+    }
+};
---
+++ firmware/firmware4leftHand_old/_main.ino
@@ -0,0 +1,121 @@
+#include<Wire.h>//This library is used for I2C communication
+#include <HardwareSerial.h>
+#define ALWAYS_CALIBRATING CALIBRATION_LOOPS == -1
+
+#define CALIB_OVERRIDE false
+#if USING_CALIB_PIN && COMMUNICATION == COMM_SERIAL && PIN_CALIB == 0 && !CALIB_OVERRIDE
+  #error "You can't set your calibration pin to 0 over usb. You can calibrate with the BOOT button when using bluetooth only. Set CalibOverride to true to override this."
+#endif
+
+// Initiate an instance for communication
+ICommunication* comm;
+int loops = 0;
+int hapticLimit = 0;
+String strFromMaster = "";
+
+/***
+Setup
+1. way to communicate: Serial or Bluetooth
+2. 
+*/
+void receiveEvent(int bytes) {
+  hapticLimit = Wire.read();//Receive value from master board
+  Serial.print("Reveived from master: "+ hapticLimit);
+}
+
+void setup() {
+  #if COMMUNICATION == COMM_BTSERIAL
+    comm = new SerialCommunication();
+    Wire.begin(12)
+    Wire.onReceive(receiveEvent);
+
+  #elif COMMUNICATION == COMM_BTSERIAL
+    comm = new BTSerialCommunication();
+  #endif  
+  comm->start(); // Start with given name, and set it to "OPEN"
+
+  setupInputs(); 
+
+  #if USING_FORCE_FEEDBACK
+    setupServoHaptics();  // attach the given pin to finger channels
+  #endif
+  
+}
+
+
+
+void loop() {
+  if (comm->isOpen()){
+    #if USING_CALIB_PIN
+    bool calibButton = getButton(PIN_CALIB) != INVERT_CALIB;
+    if (calibButton)
+      loops = 0;
+    #else
+    bool calibButton = false;
+    #endif
+    
+    bool calibrate = false;
+    if (loops < CALIBRATION_LOOPS || ALWAYS_CALIBRATING){
+      calibrate = true;
+      loops++;
+    }
+    
+    int* fingerPos = getFingerPositions(calibrate, calibButton);
+    bool joyButton = getButton(PIN_JOY_BTN) != INVERT_JOY;
+
+    #if TRIGGER_GESTURE
+    bool triggerButton = triggerGesture(fingerPos);
+    #else
+    bool triggerButton = getButton(PIN_TRIG_BTN) != INVERT_TRIGGER;
+    #endif
+
+    bool aButton = getButton(PIN_A_BTN) != INVERT_A;
+    bool bButton = getButton(PIN_B_BTN) != INVERT_B;
+
+    #if GRAB_GESTURE
+    bool grabButton = grabGesture(fingerPos);
+    #else
+    bool grabButton = getButton(PIN_GRAB_BTN) != INVERT_GRAB;
+    #endif
+
+    #if PINCH_GESTURE
+    bool pinchButton = pinchGesture(fingerPos);
+    #else
+    bool pinchButton = getButton(PIN_PNCH_BTN) != INVERT_PINCH;
+    #endif
+
+    bool menuButton = getButton(PIN_MENU_BTN) != INVERT_MENU;
+    
+    comm->output(encode(fingerPos, getJoyX(), getJoyY(), joyButton, triggerButton, aButton, bButton, grabButton, pinchButton, calibButton, menuButton));
+
+    #if USING_FORCE_FEEDBACK
+      char received[100];
+
+      
+      // String str[5] = {"","A662B0C790D600E272","A662B100C790D600E272","A662B700C790D600E272","A662B800C790D600E272"};
+      // if (str[loops%5].length()>0){
+      //   str[loops%5].toCharArray(received,str[loops%5].length());
+      //   Serial.println("before received: "+ String(received));
+      // }
+      // Serial.println("strFromMaster: "+ strFromMaster);
+      //  strFromMaster.toCharArray(received, strFromMaster.length());
+      
+      if (comm->readData(received)){
+        Serial.println("received: "+ String(received)); //received: A662B684C790D600E272
+        int hapticLimits[5];
+        //This check is a temporary hack to fix an issue with haptics on v0.5 of the driver, will make it more snobby code later
+        if(String(received).length() >= 10) {
+           decodeData(received, hapticLimits);
+           Serial.println("limits from UMD: "+ String(hapticLimits[1]) + String(hapticLimits[2]) + String(hapticLimits[3]) + String(hapticLimits[4])); // limits from UMD: 684790600272
+           writeServoHaptics(hapticLimits); 
+        }
+      }
+      else{
+        int hapticLimits[5] = {0,hapticLimit,hapticLimit,hapticLimit,hapticLimit};
+         Serial.println("limits test: "+ String(hapticLimits[1]));
+        writeServoHaptics(hapticLimits); 
+      }
+    #endif
+    delay(LOOP_TIME);
+  }
+}
